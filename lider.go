0d91986663692de166d27cfca0bb6cddc7744ed4
mgcb <melanie.corletto@sansano.usm.cl>
2021-11-05 20:11:40 -0300

    probando rabbit

--- lider.go
+++ lider.go
@@ -188,7 +188,7 @@ func failOnError(err error, msg string) {
 }
 
 func informar_jugador_eliminado(id_jugador int, ronda int){
-  conn, err := amqp.Dial("amqp://alumno:Hyp~&5xew)fRJ6!E@dist59:5672/")
+  conn, err := amqp.Dial("amqp://:@dist59:5672/")
   failOnError(err, "Failed to connect to RabbitMQ")
   defer conn.Close()
   ch, err := conn.Channel()
